dbc4636d46652c741b83ce9956b8b564575c7e1d
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-01-04 17:52:06 +0100

    tried to fetch data with groq and display it (it works)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script>
+	/**
+	 * @type {string | any[]}
+	 */
+	export let data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#if data.projects && data.projects.length}
+	<ul>
+		{#each data.projects as project}
+			<li>this is {project.name}</li>
+		{/each}
+	</ul>
+{:else}
+	<p>No data.projects found.</p>
+{/if}
