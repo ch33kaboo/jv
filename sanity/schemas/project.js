017b144845ff339ef2720ca1c047581f96b7c7f2
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-01-06 03:04:19 +0100

    embedded sanity studio in the svelte app
    briefly, the way I did it was, there is already renderStudio function that you can import from sanity, and it does the thing for you, you just need to know what to give it as parameters, so I gave it the sanity config, and installed some sanity packages, and made a schema to test with (to see if it exists in the studio), and yes everything works fine

---
+++ sanity/schemas/project.js
@@ -0,0 +1,12 @@
+export default {
+	name: 'project',
+	type: 'document',
+	title: 'Project',
+	fields: [
+		{
+			name: 'name',
+			type: 'string',
+			title: 'Name'
+		}
+	]
+};
---
+++ src/routes/studio/[...catchall]/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { renderStudio, defineConfig } from 'sanity';
+	import { deskTool } from 'sanity/desk';
+	import { visionTool } from '@sanity/vision';
+	import { onMount } from 'svelte';
+	import { schemaTypes } from '../../../../sanity/schemas';
+
+	const config = defineConfig({
+		projectId: 'u1b2ghmt',
+		title: 'jvCMS',
+		dataset: 'production',
+		apiVersion: '2023-12-18',
+		useCdn: false,
+		name: 'default',
+		plugins: [deskTool(), visionTool()],
+		schema: {
+			types: schemaTypes
+		}
+	});
+
+	let studioPage: HTMLElement;
+	function renderSanityStudio() {
+		renderStudio(studioPage, config);
+	}
+
+	onMount(renderSanityStudio);
+</script>
+
+<div class="h-screen" bind:this={studioPage} />
