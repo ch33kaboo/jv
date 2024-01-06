b160d381c3a30c3482fe6e3ae69969cb7be1daed
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-01-06 16:27:00 +0100

    add logic not to show layout on /studio route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
+	import { onMount } from 'svelte';
 	import '../app.css';
+	let path = '/';
+	onMount(() => {
+		path = window.location.pathname;
+	});
+
+	$: isStudio = path === '/studio';
 </script>
 
+{#if !isStudio}
+	here add navbar
+{/if}
 <slot />
+{#if !isStudio}
+	here add footer
+{/if}
