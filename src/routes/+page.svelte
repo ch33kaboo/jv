2076d5e816a94116559672d821bfb6e1ef1525be
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-01-08 16:42:12 +0100

    move content of +page.ts to inside the page component (+page.svelte)
    I did this because the pages do not load untill the +page.ts returns something, which is something that I do not want in this case

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,38 @@
 <script lang="ts">
-	export let data: any;
+	import { createClient } from '@sanity/client';
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const client = createClient({
+		projectId: 'u1b2ghmt',
+		dataset: 'production',
+		apiVersion: '2023-12-18',
+		useCdn: false
+	});
+
+	let load = async () => {
+		const data = await client.fetch(`*[_type == "project"]`);
+
+		if (data) {
+			return {
+				projects: data
+			};
+		}
+		return {
+			status: 500,
+			body: new Error('Internal Server Error')
+		};
+	};
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-{#if data.projects && data.projects.length}
+{#await load()}
+	wait bro
+{:then res}
 	<ul>
-		{#each data.projects as project}
+		{#each res.projects as project}
 			<li>this is {project.name}</li>
 		{/each}
 	</ul>
-{:else}
-	<p>No data.projects found.</p>
-{/if}
+{/await}
