5389524154a3e4f5c2f64f7145f5cd27edd79294
ch33kaboo <mc.ouzane@esi-sba.dz>
2025-01-07 16:53:35 +0100

    supabase setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
 	plugins: [sveltekit()],
@@ -7,5 +10,8 @@ export default defineConfig({
 		fs: {
 			allow: ['./sanity/schemas']
 		}
+	},
+	define: {
+		'process.env': process.env
 	}
 });
