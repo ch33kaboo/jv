d78db255cf0781d4200d11b56d73b201b2d07047
ch33kaboo <mc.ouzane@esi-sba.dz>
2025-01-08 13:08:54 +0100

    add content to about route

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,76 @@
-this is about page
+<div class="flex justify-center">
+	<div class="w-full prose lg:prose-xl">
+		<header>
+			<h1>Association de wilaya Sidi Bel Abbès <br /> Jeunesse Volontaire</h1>
+		</header>
+
+		<hr />
+
+		<main>
+			<section id="about">
+				<h2>À propos</h2>
+				<p>
+					Groupe de Jeunes bénévoles, engagé dans l'action sociale et la protection de
+					l’environnement.
+				</p>
+				<p><strong>Création :</strong> 07/07/2010</p>
+				<p>
+					<strong>Adresse :</strong>
+					<a
+						href="http://bing.com/maps/default.aspx?v=2&pc=FACEBK&mid=8100&where1=49%2CRue+Mahtougui+Blaha%2C+sidi+bel+abb%C3%A9s%2C+22000+Sidi+Bel+Abb%C3%A8s%2C+Algeria&FORM=FBKPL0&name=Jeunesse+Volontaire&mkt=fr-FR"
+						target="_blank">13, rue Médine Sidi Bel Abbés 22000 Algérie</a
+					>
+				</p>
+			</section>
+
+			<section id="description">
+				<h2>Description</h2>
+				<p>
+					Créée le 07/07/2010 au numéro d’enregistrement est 46/2010 par la wilaya de Sidi Bel
+					Abbès, conforme à la loi 06/12 en date du 06/01/2015, sous le N°06/2015 à but non lucratif
+					et apolitique, son but principal est la préservation de l’environnement, à l’initiative de
+					jeunes personnes qui aiment la nature ainsi que la biodiversité.
+				</p>
+			</section>
+
+			<section id="values-mission">
+				<h2>Informations générales</h2>
+				<p>
+					<strong>Valeurs :</strong> La solidarité, la créativité, le respect et la coopération.
+				</p>
+				<p>
+					<strong>Mission :</strong> Jeunesse Volontaire s’est donné pour mission un nouveau modèle de
+					société avec une nouvelle vision afin de préserver les biens communs (climat, biodiversité,
+					fonctionnement des écosystèmes).
+				</p>
+			</section>
+
+			<section id="contact">
+				<h2>Contact</h2>
+				<p>
+					<strong>Email :</strong>
+					<a href="mailto:jeunessevolontaire@yahoo.fr">jeunessevolontaire@yahoo.fr</a>
+				</p>
+				<p><strong>Téléfax :</strong> 048743279</p>
+			</section>
+
+			<section id="additional-info">
+				<h2>Informations complémentaires</h2>
+				<p>
+					<strong>Qualité :</strong> Observateur permanent au niveau des Nations Unies dans le cadre
+					de la convention des parties pour la lutte contre la désertification et la sécheresse (Octobre
+					2019).
+				</p>
+				<p>
+					<strong>Compte bancaire :</strong> <br />
+					<strong>BADR :</strong> PLACE DU 1ER NOVEMBRE Sidi Bel Abbès, 22000, Algérie<br />
+					Numéro de compte : 763000001420051
+				</p>
+				<p>
+					<strong>Le président :</strong> Ayadoun Sidahmed<br />
+					<strong>Tél :</strong> 0551006266
+				</p>
+			</section>
+		</main>
+	</div>
+</div>
