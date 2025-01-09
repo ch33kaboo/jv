c534df7a54f8867348396087e183cd2f15735d40
ch33kaboo <mc.ouzane@esi-sba.dz>
2025-01-09 14:11:31 +0100

    add two nested routes to navbar

--- src/lib/layout/NavbarElements.svelte
+++ src/lib/layout/NavbarElements.svelte
@@ -5,7 +5,14 @@
 	$: location = $page.url.pathname;
 
 	let routes = [
-		{ path: 'projects', name: 'projets' },
+		{
+			path: 'projects',
+			name: 'projets',
+			children: [
+				{ path: 'completed', name: 'projets réalisé' },
+				{ path: 'ongoing', name: 'projets en cours de réalisation' }
+			]
+		},
 		{ path: 'new', name: 'nouveauté' },
 		{ path: 'join', name: 'Nous Rejoindre' },
 		{ path: 'about', name: 'À propos' }
@@ -28,17 +35,52 @@
 		</li>
 		{#each routes as route}
 			<li
-				class="group capitalize relative cursor-pointer px-2 hover:opacity-100 {location ==
-				`/${route.path}`
+				class="group capitalize relative cursor-pointer px-2 hover:opacity-100 {location.startsWith(
+					`/${route.path}`
+				)
 					? 'opacity-100'
 					: 'opacity-60'} transition-all"
 			>
-				<a data-sveltekit-preload-data="hover" href={route.path}>{route.name}</a>
-				<span
-					class="absolute -bottom-1 left-0 h-1 {location == `/${route.path}`
-						? 'w-full'
-						: 'w-0'} rounded-full bg-slate-900 bg-opacity-90 transition-all group-hover:w-full"
-				/>
+				{#if route.children}
+					<div class="dropdown px-2">
+						<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+						<!-- svelte-ignore a11y-label-has-associated-control -->
+						<label tabindex="0" class="cursor-pointer">
+							{route.name}
+						</label>
+						<span
+							class="absolute -bottom-1 left-0 h-1 {location.startsWith(`/${route.path}`)
+								? 'w-full'
+								: 'w-0'} rounded-full bg-slate-900 bg-opacity-90 transition-all group-hover:w-full"
+						/>
+						<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+						<ul
+							tabindex="0"
+							class="dropdown-content menu p-2 shadow rounded-md w-52 bg-opacity-90 bg-gray-50 backdrop-blur-sm border top-10 -left-1 gap-2"
+						>
+							{#each route.children as child}
+								<li>
+									<a
+										class="{location == `/${route.path}/${child.path}`
+											? 'bg-gray-200'
+											: ''} active:bg-gray-200 focus:bg-gray-200"
+										data-sveltekit-preload-data="hover"
+										href={`/${route.path}/${child.path}`}
+									>
+										{child.name}
+									</a>
+								</li>
+							{/each}
+						</ul>
+					</div>
+				{:else}
+					<a data-sveltekit-preload-data="hover" href={route.path}>{route.name}</a>
+					<span
+						class="absolute -bottom-1 left-0 h-1 {location == `/${route.path}`
+							? 'w-full'
+							: 'w-0'} rounded-full bg-slate-900 bg-opacity-90 transition-all group-hover:w-full"
+					/>
+				{/if}
 			</li>
 		{/each}
 	</ul>
