3c3e3ad03b7fd246fe0420ca0b35e23113679773
ch33kaboo <mc.ouzane@esi-sba.dz>
2025-01-11 17:25:55 +0100

    protect members route (for admin only)

--- src/routes/members/+page.server.ts
+++ src/routes/members/+page.server.ts
@@ -1,8 +1,17 @@
 import { supabase } from '$lib/supabaseClient';
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { env } from '$env/dynamic/private';
 
-export async function load() {
+export const load: PageServerLoad = async ({ cookies }) => {
+	const isAuthenticated = cookies.get('authenticated') === env.ADMIN_PASSWORD;
+
+	if (!isAuthenticated) {
+		throw redirect(303, '/admin/authenticate');
+	}
+
 	const { data } = await supabase.from('users').select();
 	return {
 		users: data ?? []
 	};
-}
+};
