deb5d0d2a454608906b0b97f2beb2d9700b0d940
ch33kaboo <mc.ouzane@esi-sba.dz>
2025-01-11 22:02:12 +0100

    minor change in layout component logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,19 @@
 	import '../app.css';
 	import Navbar from '$lib/layout/Navbar.svelte';
 	import Footer from '$lib/layout/Footer.svelte';
+	import { afterNavigate } from '$app/navigation';
+
 	let path = '/';
 	onMount(() => {
 		path = window.location.pathname;
 	});
 
+	afterNavigate(() => {
+		path = window.location.pathname;
+	});
+
 	$: isStudio =
-		path.startsWith('/studio') ||
-		path.startsWith('/structure') ||
-		path.startsWith('/vision') ||
-		path.startsWith('/admin');
+		path.startsWith('/studio') || path.startsWith('/structure') || path.startsWith('/vision');
 </script>
 
 {#if !isStudio}
