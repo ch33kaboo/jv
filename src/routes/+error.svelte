27a2f3864a737103c4741598ce995481da98fdfe
ch33kaboo <mc.ouzane@esi-sba.dz>
2025-01-11 23:47:10 +0100

    override 404 error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>{$page.status} - {$page.error?.message || 'Page non trouvée'} - Jeunesse Volontaire</title>
+</svelte:head>
+
+<div class="flex flex-col items-center justify-center h-full p-8 text-center">
+	<h1>{$page.status}</h1>
+	<h2 class="mb-4">{$page.error?.message || 'Page non trouvée'}</h2>
+	<p class="text-lg">Désolé, nous n'avons pas trouvé la page que vous recherchez.</p>
+	<a href="/" class="py-2 link"> Retour à l'accueil </a>
+</div>
