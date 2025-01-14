260d5648c0db1e153b180e53f995f4ca50d65a90
ch33kaboo <mc.ouzane@esi-sba.dz>
2025-01-14 14:42:10 +0100

    minor changes

--- src/routes/projects/ongoing/+page.svelte
+++ src/routes/projects/ongoing/+page.svelte
@@ -3,8 +3,19 @@
 	export let data;
 </script>
 
+<svelte:head>
+	<title>Projets en Cours de Réalisation - Jeunesse Volontaire</title>
+	<meta
+		name="description"
+		content="Explorez les projets en cours de réalisation par Jeunesse Volontaire pour préserver l'environnement, soutenir la biodiversité et promouvoir des actions sociales durables."
+	/>
+</svelte:head>
+
 <div class="container mx-auto">
-	<h1 class="text-4xl font-bold text-center mb-8">Ongoing Projects</h1>
+	<h1 class="text-3xl font-extrabold text-start mb-2">Projets en Cours de Réalisation</h1>
+	<p class="text-neutral text-opacity-90 mb-6">
+		Voici les projets actuellement en cours de réalisation par l'Association Jeunesse Volontaire.
+	</p>
 
 	<div class="grid gap-6">
 		{#each data.projects as project}
@@ -25,7 +36,7 @@
 					<div class="mt-4 flex flex-col items-start md:flex-row md:justify-between gap-5">
 						{#if project.actions && project.actions.length > 0}
 							<div>
-								<h3 class="font-semibold mb-2">Related Actions:</h3>
+								<h3 class="font-semibold mb-2">Actions liées à ce projet:</h3>
 								<div class="flex flex-wrap gap-2">
 									{#each project.actions.slice(0, 5) as action}
 										<span class="badge badge-primary">{action.name}</span>
@@ -41,7 +52,7 @@
 						<button
 							class="btn btn-primary self-end"
 							on:click={() => console.log(`here show modal with id ${project._id}`)}
-							>see project details</button
+							>voir les détails du projet</button
 						>
 					</div>
 				</div>
