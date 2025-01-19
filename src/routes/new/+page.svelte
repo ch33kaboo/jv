b8447581ce7297ac7641350a41a50af3c86a1ecd
ch33kaboo <mc.ouzane@esi-sba.dz>
2025-01-19 23:08:51 +0100

    markup /new route

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -6,4 +6,47 @@
 	/>
 </svelte:head>
 
-this will be like facebook last post
+<div class="max-w-2xl mx-auto w-full h-full flex flex-col gap-6 justify-center items-start">
+	<h1 class="text-3xl font-extrabold">Nouveauté Title example!</h1>
+	<div
+		class="w-full h-full rounded-lg border border-neutral border-opacity-55 shadow-lg shadow-gray-300 overflow-hidden"
+	>
+		<img
+			src="https://critterfacts.com/wp-content/uploads/2018/06/Capybara-with-open-mouth.jpg"
+			alt="détail de la publication"
+			class="w-full h-full object-cover transform transition duration-300 hover:scale-105"
+		/>
+	</div>
+	<div class="text-neutral text-opacity-90 prose w-full max-w-none">
+		<p>
+			Ensemble, agissons pour un avenir durable.<br />
+			Remplissez le formulaire ci-dessous pour devenir membre de notre Association.
+		</p>
+		<p>
+			Ensemble, agissons pour un avenir durable.<br />
+			Remplissez le formulaire ci-dessous pour devenir membre de notre Association.
+		</p>
+		<p>
+			Sapiente dolor aperiam provident totam a quam voluptas labore. Minima optio atque <strong
+				>et minus debitis repellendus</strong
+			> id autem. Provident vel incidunt aut magni.
+		</p>
+		<p>
+			Sequi quisquam <em>rerum maiores </em>et est sint similique non. Voluptates ipsum fugiat modi.
+			Necessitatibus aut atque eligendi cum non laudantium est ipsam. Pariatur porro alias veritatis
+			praesentium est debitis at. Fugit consequatur doloremque qui id.
+		</p>
+		<blockquote>this is a quote text to test things</blockquote>
+		<p>
+			Rerum veritatis fugiat nostrum ipsam. Neque amet sequi nulla expedita occaecati non. <del
+				>Molestiae aliquam excepturi</del
+			> velit magnam. Sit earum fugit assumenda. Non impedit quaerat culpa nemo dolores nihil sapiente
+			sit. Aspernatur corrupti in non id quo.
+		</p>
+		<ul>
+			<li>gaga</li>
+			<li>gogo</li>
+			<li>llappo</li>
+		</ul>
+	</div>
+</div>
